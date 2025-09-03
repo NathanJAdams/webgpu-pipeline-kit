76b1c0e702cdfc70c061aa544651b698e66ecd9c
Nathan <an.email.com>
2025-09-03 23:21:39 +0100

    polish

--- src/node/dynamic-import.ts
+++ src/node/dynamic-import.ts
@@ -1,16 +1,16 @@
-export const dynamicImport = async<T = any>(library: any, name?: string): Promise<T> => {
+export const dynamicImport = async<T = any>(library: string, name?: string): Promise<T> => {
   try {
     const imported = await import(library);
-    if (name && imported[name]) {
-      return imported[name];
+    if (name) {
+      if (imported[name]) {
+        return imported[name];
+      }
+      if (imported.default?.[name]) {
+        return imported.default[name];
+      }
+      throw new Error(`Export '${name}' not found in '${library}'`);
     }
-    if (name && imported.default?.[name]) {
-      return imported.default[name];
-    }
-    if (imported.default) {
-      return imported.default;
-    }
-    return imported;
+    return imported.default ?? imported;
   } catch (error) {
     throw new Error(`Required dependency is missing, please install '${library}'.\n\n${error}`);
   }
