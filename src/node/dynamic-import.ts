39bd267078cca60686d47462fad8ba4811b85aed
Nathan <an.email.com>
2025-09-03 23:29:46 +0100

    fix dynamic imports

--- src/node/dynamic-import.ts
+++ src/node/dynamic-import.ts
@@ -1,6 +1,8 @@
-export const dynamicImport = async<T = any>(library: string, name?: string): Promise<T> => {
+export type WPKDynamicallyImportedLibrary = 'tree-sitter' | 'tree-sitter-wgsl' | 'web-naga';
+
+export const dynamicImport = async<T = any>(library: WPKDynamicallyImportedLibrary, name?: string): Promise<T> => {
   try {
-    const imported = await import(library);
+    const imported = await importLibrary(library);
     if (name) {
       if (imported[name]) {
         return imported[name];
@@ -13,5 +15,13 @@ export const dynamicImport = async<T = any>(library: string, name?: string): Pro
     return imported.default ?? imported;
   } catch (error) {
     throw new Error(`Required dependency is missing, please install '${library}'.\n\n${error}`);
+  }
+};
+
+const importLibrary = async (library: WPKDynamicallyImportedLibrary): Promise<any> => {
+  switch (library) {
+    case 'tree-sitter': return await import('tree-sitter');
+    case 'tree-sitter-wgsl': return await import('tree-sitter-wgsl');
+    case 'web-naga': return await import('web-naga');
   }
 };
