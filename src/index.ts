b6e5ffb1aef2c9c067cc9c8f3ae1a7c7f33046f8
Nathan <an.email.com>
2025-09-26 18:15:22 +0100

    export missing type

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export type {
   WPKMeshTemplate,
   WPKMeshTemplateMap,
   WPKMeshParametersDeclaration,
+  WPKMouseButton,
   WPKPeripheralEventHandlers,
   WPKPipelineOptions,
   WPKComputeShader,
